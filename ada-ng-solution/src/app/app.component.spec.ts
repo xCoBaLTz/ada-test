4a6fa1dff9d6767c23294cd5873e27ba3e97860b
Seanan Chabra <seananchabra@hotmail.com>
2020-11-02 21:19:19 -0500

    cleaned up some tests and lint

--- ada-ng-solution/src/app/app.component.spec.ts
+++ ada-ng-solution/src/app/app.component.spec.ts
@@ -1,16 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-      providers: [
+      imports: [
         HttpClientTestingModule
       ]
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
